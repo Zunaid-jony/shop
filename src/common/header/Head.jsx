30302433ccee3c42ce5ca6a40bf0f4b5404fc6fe
Zunaid-jony <jony35666666@gmail.com>
2022-06-28 00:04:18 +0600

    project done

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +880 1627392810</label>
+            <i className='fa fa-envelope'></i>
+            <label> md.jony.soft@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+           
+          
+            <label></label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
